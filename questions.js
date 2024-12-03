e8c69af46dd60da240bca654633bb70633e1cb3e
Monika <mciskowska@gmail.com>
2024-12-03 13:40:44 +0000

    created question1

--- questions.js
+++ questions.js
@@ -4,5 +4,90 @@ const questions = [
   // Add your questions here
 ];
 
+const question1 = {
+  question_text: "How can you create a new branch directory to work in?",
+  question_type: "multiple_choice",
+  options: ["git branch", "git check", "git checkout", "git status"],
+  correct_option: options[0],
+  correct_explanation: "We use Git branches to work on different features or changes separately without affecting the main codebase."
+}
+
+const question2 = {
+  question_text: 
+  question_type: "multiple_choice",
+  options: 
+  correct_option:
+  correct_explanation:
+}
+
+
 // Don't worry about this, we're just exporting the questions
 module.exports = questions;
+
+
+/*
+const question1 = {
+  question_text: 
+  question_type: "multiple_choice",
+  options: 
+  correct_option:
+  correct_explanation:
+}
+  */
+/*
+Add your questions as objects in the array, following the schema above
+Make sure your questions:
+Are about Week 1 content
+Have 4 options each
+Have clear explanations for the correct answers
+Follow the exact schema format
+
+
+Questions: 
+Which language do you use to style website
+Which year JS has been developed 
+What is a flow diagram for
+What is MVP
+
+Questions
+
+
+
+1
+How can you create a new branch directory to work in?
+- git branch V
+- git check
+- git checkout
+- git status
+
+
+2
+When giving feedback is best to 
+- State your subjective opinions and convince the other person no matter what.
+- Rush through the topic to bamboozle the feedback receiver
+- Adopt an assertive approach to understand and give better advice. V
+- Try to say the least possible.
+
+3
+In pair programming we can see the relation between the developers as:
+- Batman and Robin
+- Director and coach
+- Driver and navigator
+- Fugitive and police.
+
+4
+Before asking for help with an issue make sure to:
+ - Understand what is you are trying to ask
+ - Look worried to give a sense of urgency
+ - State that you can't go further if it's not solved.
+ - Make a list of demands.
+
+
+ 5
+The method of coming back to the same topic to reinforce it into our memory is called:
+Memory palace
+Spaced Repetition
+Memory gymnastics
+Neural networking
+
+*/
